1bab9b48195d5aefa69ff3433ca6855f5bb01bd8
Sushanth <sushanthrebel@gmailcom>
2025-03-17 00:35:16 +0530

    Adding exception

--- src/main/java/com/demo/example/student_library_management_system/controller/StudentController.java
+++ src/main/java/com/demo/example/student_library_management_system/controller/StudentController.java
@@ -3,7 +3,9 @@ package com.demo.example.student_library_management_system.controller;
 import com.demo.example.student_library_management_system.model.Student;
 import com.demo.example.student_library_management_system.requestdto.StudentRequestDto;
 import com.demo.example.student_library_management_system.service.StudentService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,9 +18,13 @@ public class StudentController {
     private StudentService studentService;
 
     @PostMapping("/save")
-    public String saveStudent(@RequestBody  StudentRequestDto studentRequestDto){
-        String response = studentService.saveStudent(studentRequestDto);
-        return response;
+    public ResponseEntity<?> saveStudent(@RequestBody  StudentRequestDto studentRequestDto){
+        try{
+            String response = studentService.saveStudent(studentRequestDto);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("issue witth data");
+        }
     }
 
     @GetMapping("/findDept")
